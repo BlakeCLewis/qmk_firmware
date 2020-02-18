e8ec9896c1e20905e5edf84f9f900b6798a58193
Lewis, Blake C <lewisbc@umsystem.edu>
2020-02-18 17:39:47 -0600

    cformat

--- keyboards/kc60se/kc60se.c
+++ keyboards/kc60se/kc60se.c
@@ -16,13 +16,11 @@
 
 #include QMK_KEYBOARD_H
 
-void matrix_init_kb(void){
-    setPinOutput(B2);
-}
+void matrix_init_kb(void) { setPinOutput(B2); }
 
 bool led_update_kb(led_t led_state) {
     bool res = led_update_user(led_state);
-    if(res) {
+    if (res) {
         writePin(B2, !led_state.caps_lock);
     }
     return res;
